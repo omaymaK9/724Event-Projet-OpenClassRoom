bd0158d9eef28d860c1c89a9942172c790508d74
oma <125909507+omaymaK9@users.noreply.github.com>
2023-08-21 12:37:43 +0200

    Update index.js

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -70,33 +70,3 @@ Form.defaultProps = {
 
 export default Form;
 
-
-/* e tri par date est bien par ordre décroissant ;
-le slider envoie bien vers la slide suivante ;
-le filtre par type d’évènement fonctionne ;
-le helper getMonth renvoie le bon mois ;
-la “callback” onSuccess est exécutée ;
-la value last est bien implémentée pour que le bon évènement s’affiche dans le footer. */ 
-
-/* Critères d’évaluation 
-Débugger un site web grâce aux Chrome DevTools
-Le code débuggé peut être validé si : 
-
-❒ Tous les tests initialement implémentés sont valides :
-
-When Event is created › and we select a category › a filtered list is displayed.
-When an event card is created › a title, a label and a month are displayed.
-When Slider is created › a list of cards is displayed.
-When Form is created › and a click is triggered on the submit button › the success action is called.
-When Form is created › and a click is triggered on the submit button › the success message is displayed.
-❒ Aucune erreur n'est produite lors de l'utilisation du site.
-
-❒ Le comportement de chaque composant est en adéquation avec les besoins du client.
-
-❒ Les 6 bugs détaillés plus haut sont corrigés :
-
-le tri par date est bien par ordre décroissant ;
-le slider envoie bien vers la slide suivante ;
-le filtre par type d’évènement fonctionne ;
-le helper getMonth renvoie le bon mois ;
-la “callback” onSuccess est exécuté */ 
